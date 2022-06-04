a9d211514e636252cc6986d9aad95f19cfc6988c
joaogarcia13 <joaobernardogarcia@gmail.com>
2022-06-04 18:19:59 +0100

    foto a funcionar

--- app/src/main/java/com/example/projecto_dadm/registar_item_activity.java
+++ app/src/main/java/com/example/projecto_dadm/registar_item_activity.java
@@ -2,33 +2,39 @@ package com.example.projecto_dadm;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import com.pranavpandey.android.dynamic.toasts.DynamicToast;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 public class registar_item_activity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Bitmap iconeImg;
-    private String currentPhotoPath;
-    private Image foto;
+
+    private String FotoB64 = "";
+    private String thumbnail = "";
+    private Uri fileUri;
+    private String nome;
+    private String descricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar_item);
+        nome = findViewById(R.id.reg_item_nome_input).toString();
 
     }
     @Override
@@ -41,58 +47,65 @@ public class registar_item_activity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            iconeImg = (Bitmap) extras.get("data");
-            //TODO ja tens o icon falta a imagem toda para ir para o parametro foto
-        }
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                //converte a imagem que esta no caminho getOutputMediaFile() num bitmap e converte para Base64
+                try{
+                    InputStream imageStream = getContentResolver().openInputStream(fileUri);
+                    Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    FotoB64 = encodeImage(selectedImage);
+                    Log.d("imagem:", FotoB64);
+                }catch (Exception e) {
+                    DynamicToast.makeError(registar_item_activity.this, "Ocorreu um erro ao obter a imagem. Por favor tente novamente.").show();
+                    e.printStackTrace();
+                }
+            }
     }
 
     public void Tirar_Foto_regItem(View v){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            //startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                } catch (IOException ex) {
-                    Log.e("fds","caralho!");
-                }
-                if (photoFile != null) {
-                    Uri photoURI = FileProvider.getUriForFile(this,
-                            "com.example.android.fileprovider",
-                            photoFile);
-                    //intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                }
-        }catch (Exception e){
-            DynamicToast.makeError(registar_item_activity.this, "Ocorreu um erro ao abrir a câmara, tente novamente.").show();
-            Log.e("error:", String.valueOf(e));
-        }
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri();
+        i.putExtra( MediaStore.EXTRA_OUTPUT, fileUri );
+        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
     }
 
     public void regItem_Confirmar(View v){
+        Log.d("d3",nome);
+    }
+
+    public void regItem_Reset(View v){
 
     }
 
-    public void redItem_Reset(View v){
+    public void regItem_menu(View v){
 
     }
 
-    public void redItem_menu(View v){
-
+    private String encodeImage(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encImage;
     }
 
-    private File createImageFile() throws IOException {
+    //vai buscar o Uri da foto criado em getOutputMediaFile()
+    private Uri getOutputMediaFileUri(){
+        return FileProvider.getUriForFile(registar_item_activity.this, BuildConfig.APPLICATION_ID + ".provider",getOutputMediaFile());
+    }
+
+    //cria um ficheiro para guardar imagem
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("Error: ", "Nao foi possivel criar o directorio.");
+                return null;
+            }
+        }
+        // Cria um nome para a imagem
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+        return mediaFile;
     }
 }
